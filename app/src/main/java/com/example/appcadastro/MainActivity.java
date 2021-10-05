6b0903f56ea602324f7bcbb3bdef9684da985446
Lucca465 <lucca465@gmail.com>
2021-10-05 18:38:09 -0300

    Criando o evento click para o botões da tela principal

--- app/src/main/java/com/example/appcadastro/MainActivity.java
+++ app/src/main/java/com/example/appcadastro/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.appcadastro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void botaoCriarContaClick(View view){
+        Intent intent = new Intent();
+        intent.setClass(getApplicationContext(),TelaCadastro.class);
+        startActivity(intent);
+    }
+
+    public void textoLoginClick(View view){
+        Intent intent = new Intent();
+        intent.setClass(getApplicationContext(),TelaLogin.class);
+        startActivity(intent);
     }
 }
